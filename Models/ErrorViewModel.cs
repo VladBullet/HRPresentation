d5e1a3b909cf1657059a169f8900473d2b95f726
VladBullet <vladone1996@gmail.com>
2020-07-04 13:45:52 +0300

    add app to repo

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HRPresentation.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
